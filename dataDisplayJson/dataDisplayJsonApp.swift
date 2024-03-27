0a28514a5ce149d8136324cffd24692a63362013
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-03-27 15:55:44 -0500

    Initial Commit

---
+++ dataDisplayJson/dataDisplayJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  dataDisplayJsonApp.swift
+//  dataDisplayJson
+//
+//  Created by Abraham Morales Arroyo on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct dataDisplayJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
